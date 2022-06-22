e818122fffb5b2ec64a35fb57e5459b13ad32bdd
Katarzyna Wietecha <wietecha.katarzyna@gmail.com>
2022-06-22 23:26:28 +0200

    Convert variables for books to an array

--- src/pl/javastart/library/app/Library.java
+++ src/pl/javastart/library/app/Library.java
@@ -6,14 +6,15 @@ public class Library {
     public static void main(String[] args) {
         final String appName = "Library v0.1";
 
-        Book book1 = new Book("Efekt motyla", "Kamil Celubski", 2008, 141, "Złote Myśli", "9788375821994");
-        Book book2 = new Book("Jesteś cudem", "Regina Brett", 2022, 256, "Insignis", "9788367323055");
-        Book book3 = new Book("Finansowa Forteca", "Marcin Iwuć", 2020, 800, "Finanse Bardzo Osobiste");
+        Book [] books = new Book[1000];
+        books[0] = new Book("Efekt motyla", "Kamil Celubski", 2008, 141, "Złote Myśli", "9788375821994");
+        books[1] =  new Book("Jesteś cudem", "Regina Brett", 2022, 256, "Insignis", "9788367323055");
+        books[2] =  new Book("Finansowa Forteca", "Marcin Iwuć", 2020, 800, "Finanse Bardzo Osobiste");
 
         System.out.println(appName);
         System.out.println("Available items:");
-        System.out.println(book1.getBookInfo());
-        System.out.println(book2.getBookInfo());
-        System.out.println(book3.getBookInfo());
+        System.out.println(books[0].getBookInfo());
+        System.out.println(books[1].getBookInfo());
+        System.out.println(books[2].getBookInfo());
     }
 }
