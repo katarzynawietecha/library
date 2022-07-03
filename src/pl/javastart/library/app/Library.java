e7e90f13e5036b5b75bd7085b014288c6de5914c
Katarzyna Wietecha <wietecha.katarzyna@gmail.com>
2022-07-03 18:53:01 +0200

    Create books from user input

--- src/pl/javastart/library/app/Library.java
+++ src/pl/javastart/library/app/Library.java
@@ -1,20 +1,21 @@
 package pl.javastart.library.app;
 
+import pl.javastart.library.io.DataReader;
 import pl.javastart.library.model.Book;
 
 public class Library {
     public static void main(String[] args) {
         final String appName = "Library v0.1";
+        DataReader dataReader = new DataReader();
 
         Book [] books = new Book[1000];
-        books[0] = new Book("Efekt motyla", "Kamil Celubski", 2008, 141, "Złote Myśli", "9788375821994");
-        books[1] =  new Book("Jesteś cudem", "Regina Brett", 2022, 256, "Insignis", "9788367323055");
-        books[2] =  new Book("Finansowa Forteca", "Marcin Iwuć", 2020, 800, "Finanse Bardzo Osobiste");
+        books[0] = dataReader.readAndCreateBook();
+        books[1] = dataReader.readAndCreateBook();
+        dataReader.closeScanner();
 
         System.out.println(appName);
         System.out.println("Available items:");
         System.out.println(books[0].getBookInfo());
         System.out.println(books[1].getBookInfo());
-        System.out.println(books[2].getBookInfo());
     }
 }
